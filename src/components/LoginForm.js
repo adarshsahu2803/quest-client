9f47e2c416b8fa697b84bda2f14f319e7e1a07b0
Adarsh Sahu <adarshsahu1077@gmail.com>
2024-01-11 07:57:26 +0530

    Update LoginForm.js

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -22,7 +22,7 @@ const LoginForm = () => {
         console.log(formLoginData);
 
         try {
-            const response = await fetch(`${SERVER_URL}/login`, {
+            const response = await fetch("https://quest-server.onrender.com/login", {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
